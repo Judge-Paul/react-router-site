6aee9b3463115db1b54f0bc78c92cb8b6058e67d
Judge-Paul Ogebe <110723341+Judge-Paul@users.noreply.github.com>
2023-01-26 04:14:34 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
